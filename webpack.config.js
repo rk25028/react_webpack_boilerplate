f391dd04f0896240d09ece02d955d60eca2ccdfc
rk25028 <36230401+rk25028@users.noreply.github.com>
2020-01-03 13:46:59 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+              }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:8090',
+            apiKey: 'f330c3390cf5c9fa0e23f5b2896dacf158d5bcc0'
+        })
+    }
+}
